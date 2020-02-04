1c39a4c6ee8ef5b34b1cb68088a7aacf56dee8b0
Vish Mayer <vish.mayer@gmail.com>
2020-02-04 16:07:21 +0000

    TDD a model for a note

---
+++ note-model-test.js
@@ -0,0 +1,12 @@
+
+
+(function testTextStoredInNote() {
+  var note = new Note("Hello");
+  assert.isTrue(note.text === "Hello");
+
+})();
+
+(function testReturnsText() {
+  var note = new Note("Hi");
+  assert.isTrue(note.getText() === "Hi");
+})();
---
+++ note-model.js
@@ -0,0 +1,13 @@
+
+
+(function(exports) {
+  function Note(text) {
+    this.text = text;
+  }
+
+  Note.prototype.getText = function() {
+    return this.text;
+  }
+
+  exports.Note = Note;
+}) (this);
